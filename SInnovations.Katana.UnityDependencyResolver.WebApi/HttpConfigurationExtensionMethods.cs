55b29c7e75cc17a414e4dff35ed7fb558d85f07f
Poul Kjeldager Sørensen <pks@s-innovations.net>
2014-10-18 18:23:00 +0200

    Added  a method for easy adding the dpr to httpconfiguration

---
+++ SInnovations.Katana.UnityDependencyResolver.WebApi/HttpConfigurationExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿using SInnovations.Katana.UnityDependencyResolver.WebApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Web.Http
+{
+    public static class HttpConfigurationExtensionMethods
+    {
+
+        public static void AddKatanaUnityDependencyResolver(this HttpConfiguration configuration)
+        {
+            configuration.MessageHandlers.Insert(0, new KatanaUnityDependencyResolver());
+        }
+    }
+}
